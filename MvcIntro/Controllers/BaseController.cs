eb13bc836243a90d5b45308ecfd7ef013d304fc1
Matrix hunter <marybakayeva@gmail.com>
2022-05-10 17:32:52 +0300

    intro commit

---
+++ MvcIntro/Controllers/BaseController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcIntro.Models.CustomUser;
+
+namespace MvcIntro.Controllers
+{
+    public class BaseController : Controller
+    {
+        protected virtual new CustomUser User
+        {
+            get { return HttpContext.User as CustomUser; }
+        }
+    }
+}
---
+++ MvcIntro/Models/ViewModel/UserRegisterModel.cs
@@ -0,0 +1,17 @@
+﻿using MvcIntro.Validators;
+using System.ComponentModel.DataAnnotations;
+
+namespace MvcIntro.Models.ViewModel
+{
+    public class UserRegisterModel
+    {
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        [StrongPassword]
+        public string Password { get; set; }
+        [Required]
+        [Compare("Password")]
+        public string PasswordRepeat { get; set; }
+    }
+}
